1045386c814c231f7995b6b78cd2e26eef52675e
ElusiveFox <44260690+ElusiveFoxie@users.noreply.github.com>
2020-10-25 17:01:21 +0100

    Create simpleHTTP.java

---
+++ simpleHTTP.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class simpleHTTP {
+
+    public static void main(String[] args) throws IOException {
+
+        URL url = new URL("url");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        String response = FullResponseBuilder.getFullResponse(con);
+        con.disconnect();
+
+        System.out.println(response);
+
+    }
+}
